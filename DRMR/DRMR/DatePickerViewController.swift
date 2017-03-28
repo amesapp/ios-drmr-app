5b300f4a4ae8b34b6ce222053330cab8023ee2d7
Enzo <e.amespizarro@gmail.com>
2017-03-27 21:06:14 -0400

    shows date picker

--- DRMR/DRMR/DatePickerViewController.swift
+++ DRMR/DRMR/DatePickerViewController.swift
@@ -10,18 +10,33 @@ import UIKit
 
 class DatePickerViewController: UIViewController {
     
+    @IBOutlet weak var popUpView: UIView!
+    
+    @IBOutlet weak var selectDatePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        popUpView.layer.cornerRadius = 10
+        selectDatePicker.layer.cornerRadius = 10
+        popUpView.layer.masksToBounds =  true
         // Dispose of any resources that can be recreated.
     }
+    
+    
+//    @IBAction func pickerValueChanged(_ sender: Any) {
+//        let formatter =
+//        
+//        
+//    }
     
     @IBAction func onTapDone(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
-
+    
+    
 }
